bf4d3edb7f486674f706186cac4fc16093b2740e
elonqian <elonqian216@gmail.com>
2026-05-29 17:34:04 +0800

    fix: 删除 CreateProjectResult 重复定义（rebase 引入的冲突）

--- server/src/store/store_types.rs
+++ server/src/store/store_types.rs
@@ -296,12 +296,6 @@ pub struct ProjectSummary {
 }
 
 #[derive(Debug, Clone)]
-pub struct CreateProjectResult {
-    pub project: ProjectSummary,
-    pub reused_existing: bool,
-}
-
-#[derive(Debug, Clone)]
 pub struct ProjectAccess {
     pub id: String,
     pub name: String,
