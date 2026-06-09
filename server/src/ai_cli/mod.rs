f052dfef114918a73512474dfdf5fc692567251d
一龙ai助手 <elonqian216@gmail.com>
2026-06-09 12:09:19 +0800

    fix: 修复 WIP 改动引起的编译错误（AssistantMessage/Done node_id 字段 + list_public_projects 参数）

--- server/src/ai_cli/mod.rs
+++ server/src/ai_cli/mod.rs
@@ -821,7 +821,6 @@ async fn run_via_pc_agent(
                             text: text.clone(),
                             model_used: Some(display_model.clone()),
                             stream_id: Some(stream_id.clone()),
-                            node_id: Some(agent_id.to_string()),
                         }.to_json());
                     } else {
                         let _ = tx.send(WsMessage::AssistantChunk {
@@ -842,7 +841,6 @@ async fn run_via_pc_agent(
                         text: text.clone(),
                         model_used: Some(display_model.clone()),
                         stream_id: Some(stream_id.clone()),
-                        node_id: Some(agent_id.to_string()),
                     }.to_json());
                 } else {
                     let _ = tx.send(WsMessage::AssistantChunk {
@@ -871,7 +869,6 @@ async fn run_via_pc_agent(
                             text: reply,
                             model_used: Some(display_model.clone()),
                             stream_id: None,
-                            node_id: Some(agent_id.to_string()),
                         }.to_json());
                     }
                     let _ = tx.send(WsMessage::Done {
