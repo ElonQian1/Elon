faff642161a5e5ae947bc08e0c58b8abe769c2d0
志伟 <siwmm@163.com>
2026-07-04 20:26:33 +0800

    fix(android): 放大顶部新增按钮图标

--- android/app/src/main/kotlin/com/elon/app/MainNavigationController.kt
+++ android/app/src/main/kotlin/com/elon/app/MainNavigationController.kt
@@ -1158,6 +1158,6 @@ internal class MainNavigationController(
         const val PROJECT_TOP_INDICATOR_HEIGHT_PX = 6
         const val PROJECT_TOP_INDICATOR_BOTTOM_PX = 18
         const val PROJECT_ADD_BUTTON_SIZE_PX = 156
-        const val PROJECT_ADD_BUTTON_PADDING_PX = 46
+        const val PROJECT_ADD_BUTTON_PADDING_PX = 28
     }
 }
